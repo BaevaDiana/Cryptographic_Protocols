d405a18ee32a03d703eeeeb447ac2f81e406d487
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2024-03-12 12:14:23 +0300

    лр 5 полностью + отчёт

--- LW_5.py
+++ LW_5.py
@@ -1,12 +1,17 @@
+# реализация теста Ферма
+
 import random
 
-
-def is_prime_fermat(n, k):
+def is_prime(n, k=5):
     """
-    Выполняет тест Ферма для заданного числа.
-    :param n: Число, которое нужно проверить
-    :param k: Количество итераций (тестов)
-    :return: True, если n вероятно простое, иначе False
+    Выполняет тест Ферма на простоту числа.
+
+    Args:
+        n (int): Число для проверки на простоту.
+        k (int, optional): Количество тестов. По умолчанию 5.
+
+    Returns:
+        bool: True, если число вероятно простое, иначе False.
     """
     if n <= 1:
         return False
@@ -20,19 +25,48 @@ def is_prime_fermat(n, k):
 
     return True
 
+def find_composite_numbers(limit, num_tests):
+    """
+    Находит составные числа до заданного предела.
 
+    Args:
+        limit (int): Верхний предел поиска.
+        num_tests (int): Количество тестов для каждого числа.
 
-# Выполняем тесты Ферма для чисел от 1 до 100
-print("Введите количество тестов:")
-k = int(input())
-print("Введите количество чисел:")
-num_numbers = int(input())
-# Выполняем тесты Ферма для чисел от 1 до 100
-for num in range(2, num_numbers):
-    is_prime = is_prime_fermat(num, k)
-    if is_prime:
-        print(f"{num} вероятно простое")
-    else:
-        print(f"{num} вероятно составное")
+    Returns:
+        list: Список составных чисел.
+    """
+    composite_numbers = []
+    for i in range(2, limit + 1):
+        if not is_prime(i, num_tests):
+            composite_numbers.append(i)
+    return composite_numbers
 
+def composite_numbers_test(composite_numbers, num_tests):
+    """
+    Проверяет составные числа, чтобы увидеть результат множественных тестов.
 
+    Args:
+        composite_numbers (list): Список составных чисел.
+        num_tests (int): Количество тестов.
+
+    Returns:
+        dict: Словарь с составными числами в качестве ключей и результатами тестов в качестве значений.
+    """
+    results = {}
+    for num in composite_numbers:
+        test_result = is_prime(num, num_tests)
+        results[num] = test_result
+    return results
+
+# поиск составных чисел до одного миллиарда
+limit = 1000
+composite_numbers = find_composite_numbers(limit, num_tests=1)
+
+# проверка составных чисел с 2 до 100 тестов
+test_results = composite_numbers_test(composite_numbers, num_tests=100)
+
+# вывода результата
+for num, result in test_results.items():
+    print(f"Число {num}: Результат первого теста: {'Простое' if result else 'Составное'}, "
+          f"результат 100 тестов: {'Простое' if is_prime(num, 100) else 'Составное'}")
